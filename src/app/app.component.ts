6d29fbe40c7a81134b04f46e0ddc0e8a870beabd
MAMIYASSINE <mamiy463@gmail.com>
2023-12-12 18:46:59 +0100

    tp1_ex4

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, AfterViewInit, ViewChild } from
+'@angular/core';
+@Component({
+selector: 'app-root',
+template: `<input #input type="text" (input)="textInput($event)"
+value=""/>
+<hr>
+Upper-Case: {{upperCase}}
+<br/>
+Lower-Case: {{lowerCase}}`,
+styles: [] })
+export class AppComponent implements AfterViewInit{
+upperCase: string= '';
+lowerCase: string = '';
+@ViewChild('input')
+  inputBox: any;
+
+extInput(event: { target: { value: string; }; }){
+this.upperCase = event.target.value.toUpperCase();
+this.lowerCase = event.target.value.toLowerCase();
+}
+ngAfterViewInit() {
+this.inputBox.nativeElement.focus()
+}}
